d175a56357995f363ee57b13d55ae989c2664ed1
kerBiy <alexandrubalta222@gmail.com>
2023-10-22 12:59:36 +0300

    ok

--- FP/lab 2/2.py
+++
@@ -1,9 +0,0 @@
-# Se da data nașterii (zi/luna/an), determinați vârsta persoanei in zile.
-from datetime import datetime
-
-brithday = input("Enter Birthday (day/month/year): ")
-brithday = datetime.strptime(brithday, "%d/%m/%Y")
-now = datetime.now()
-difference = now - brithday
-
-print("The age in days is: ", difference.days)
--- FP/lab 2/3.py
+++
@@ -1,9 +0,0 @@
-# Determina o data calendaristica (sub forma an, luna, zi) pornind de la doua numere întregi care reprezintă anul si numărul de ordine al zilei in anul respectiv.
-from datetime import datetime, timedelta
-
-year = int(input("Enter the year: "))
-order_num = int(input("Enter the order of the day: "))
-
-date = datetime(year, 1, 1)
-date += timedelta(days=order_num - 1)
-print(f"The date is: {date.day}/{date.month}/{date.year}")
