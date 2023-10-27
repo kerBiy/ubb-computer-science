6b3a33f377129629468d910405687e58f9e23107
kerBiy <alexandrubalta222@gmail.com>
2023-10-27 19:31:44 +0300

    lab3 adunarea

--- Arhitectura-Sistemelor-de-Calcul/lab3/adunari/cuSemn.asm
+++ Arhitectura-Sistemelor-de-Calcul/lab3/adunari/cuSemn.asm
@@ -4,17 +4,55 @@ global  start
 extern  exit 
 import  exit msvcrt.dll
 
+; d - (a + b + c) - (a + a)
 segment data use32 class=data 
-    a dd 125
-	b db 2
-	c dw 15
-	d db 200
-	e dq 80
+    a db 1
+	b dw 2
+	c dd 5
+	d dq 10
+	; exp = ?
 
-; (d - c) + (b - a) - (b + b + b)
 segment code use32 class=code 
 start: 
-	
-	
+	; ax <- a + b
+	mov al, [a]
+	cbw
+	mov bx, [b]
+	add ax, bx
+
+	; eax <- a + b + c
+	cwde
+	add eax, [c]
+
+	; ebx <- eax
+	push eax
+	pop ebx
+
+	; al <- a + a
+	mov al, [a]
+	add al, [a]
+
+	; eax <- (a + b + c) - (a + a)
+	cbw
+	cwde
+	sub ebx, eax
+
+	push ebx
+	pop eax
+
+	; ecx:ebx <- d - (a + b + c) - (a + a)
+	mov ebx, [d]
+	mov ecx, [d + 4]
+
+	cdq
+	sub ebx, eax
+	sbb ecx, edx
+
+	; edx:eax <- ecx:ebx
+	push ecx
+	push ebx
+	pop eax
+	pop edx
+
 	push dword 0
 	call [exit] 
---
+++ Arhitectura-Sistemelor-de-Calcul/lab3/adunari/faraSemn.asm
@@ -0,0 +1,61 @@
+bits 32 
+global  start 
+
+extern  exit 
+import  exit msvcrt.dll
+
+; (d - c) + (b - a) - (b + b + b)
+segment data use32 class=data 
+    a db 1
+	b dw 2
+	c dd 5
+	d dq 10
+	; exp = ?
+
+segment code use32 class=code 
+start: 
+	; ecx:ebx <- d - c
+	mov eax, [d]
+	mov edx, [d + 4]
+
+	push edx
+	push eax
+	pop ebx
+	pop ecx
+
+	mov eax, [c]
+	mov edx, 0
+
+	sub ebx, eax
+	sbb ecx, edx
+
+	; reset de eax for the last step
+	mov eax, 0
+
+	; ax <- b - a	
+	mov ax, [b]
+	mov dl, [a]
+	mov dh, 0
+	sub ax, dx
+
+	; dx <- b + b + b
+	mov dx, [b]
+	add dx, [b]
+	add dx, [b]
+
+	; ax <- (b - a) - (b + b + b)
+	sub ax, dx
+
+	; ecx:ebx <- (d - c) + (b - a) - (b + b + b)
+	mov edx, 0
+	add ebx, eax
+	adc ecx, edx
+
+	; edx:eax <- ecx:ebx
+	push ecx
+	push ebx
+	pop eax
+	pop edx
+
+	push dword 0
+	call [exit] 
