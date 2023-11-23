98363debe6b22d4eade4dde5fd040c1fb9bf4caf
kerBiy <alexandrubalta222@gmail.com>
2023-11-23 17:33:33 +0200

    finsish 11 v2

--- Arhitectura-Sistemelor-de-Calcul/lab6/11v2.asm
+++ Arhitectura-Sistemelor-de-Calcul/lab6/11v2.asm
@@ -8,7 +8,7 @@ import  exit msvcrt.dll
 ;  - B1: contine ca elemente partea superioara a cuvintelor superioare din A
 ;  - B2: contine ca elemente partea inferioara a cuvintelor inferioare din A
 segment data use32 class=data 
-    sir  dw  10abcb0h, 051ad2h, 74120ca1h
+    sir  dd  10abcb0h, 051ad2h, 74120ca1h
 	len equ ($ - sir) / 4 ; lungimea sirului
     b1 times len db 0 ; 01, 00, 74
     b2 times len db 0 ; b0, d2, a1
@@ -17,7 +17,7 @@ segment code use32 class=code
     start: 
         mov esi, sir
         mov edi, b1
-        cdf ; pargcurgem de la st la dr
+        cld ; pargcurgem de la st la dr
         mov ecx, len ; punem lungimea in ecx
         mov edx, 0 ; iterator pe b2 
         
