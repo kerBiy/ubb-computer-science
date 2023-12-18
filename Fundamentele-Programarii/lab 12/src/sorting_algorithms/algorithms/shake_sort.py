def24ae46b2ede705472e17ec00c5c46941b1b87
kerBiy <alexandrubalta222@gmail.com>
2023-12-18 22:52:10 +0200

    made a new shake sort that is more commentated

--- Fundamentele-Programarii/lab 12/src/sorting_algorithms/algorithms/shake_sort.py
+++ Fundamentele-Programarii/lab 12/src/sorting_algorithms/algorithms/shake_sort.py
@@ -6,10 +6,11 @@ class ShakeSort(GenericSort):
         super().__init__(iterable, key, reverse)
 
     def sort(self) -> None:
-        up = range(len(self.iterable) - 1)
+        forward = range(len(self.iterable) - 1)
         while True:
-            for indices in (up, reversed(up)):
+            for indices in (forward, list(reversed(forward))):
                 swapped = False
+
                 for i in indices:
                     if not self._cmp(self.iterable[i], self.iterable[i + 1]):
                         self.iterable[i], self.iterable[i + 1] = (
@@ -17,5 +18,45 @@ class ShakeSort(GenericSort):
                             self.iterable[i],
                         )
                         swapped = True
+
                 if not swapped:
                     return
+
+    # def sort(self) -> None:
+    #     start = 0
+    #     end = len(self.iterable) - 1
+
+    #     swapped = True
+    #     while swapped:
+    #         # Reset the swapped flag
+    #         swapped = False
+
+    #         # Perform the forward pass (left to right)
+    #         for i in range(start, end):
+    #             if not self._cmp(self.iterable[i], self.iterable[i + 1]):
+    #                 self.iterable[i], self.iterable[i + 1] = (
+    #                     self.iterable[i + 1],
+    #                     self.iterable[i],
+    #                 )
+    #                 swapped = True
+
+    #         # If nothing moved, the array is already sorted
+    #         if not swapped:
+    #             return
+
+    #         # Reset the swapped flag
+    #         swapped = False
+
+    #         # Perform the backward pass (right to left)
+    #         for i in reversed(range(start, end)):
+    #             if not self._cmp(self.iterable[i], self.iterable[i + 1]):
+    #                 self.iterable[i], self.iterable[i + 1] = (
+    #                     self.iterable[i + 1],
+    #                     self.iterable[i],
+    #                 )
+    #                 swapped = True
+
+    #         # Decrease the end point
+    #         end -= 1
+    #         # Increase the starting point
+    #         start += 1
